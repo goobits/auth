54d01aaba78107de2a0dd540559ddf42c5b89eb5
Miko Meow <101564+mudcube@users.noreply.github.com>
2026-02-15 14:53:24 -0800

    bind dev server to vm port

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		// vm.yaml reserves ports 3580-3589 and binds them to the host.
+		host: true, // 0.0.0.0
+		port: Number(process.env.PORT) || 3580,
+		strictPort: true
+	},
+	preview: {
+		host: true,
+		port: Number(process.env.PORT) || 3580,
+		strictPort: true
+	}
 });
